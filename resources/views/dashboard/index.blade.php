404b872b283df98fbb1fe0b8c82cc4284ede1b66
Chaidirrahman <90771243+chaidir10@users.noreply.github.com>
2022-10-09 18:47:57 +0700

    REVISI 1

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -2,6 +2,6 @@
 
 @section('container')
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h4><span class="badge bg-info"><span data-feather="coffee" class="mb-1"></span> Welcome back!, {{auth()->user()->name}}</span></h4>
+        <h4><span class="badge bg-info"><span data-feather="coffee" class="mb-1"></span> Welcome BUTTck!, {{auth()->user()->name}}</span></h4>
     </div>
 @endsection
